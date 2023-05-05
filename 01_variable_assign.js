8f8855c7146d8ca6dc6149a073a8cc3ad2735555
pawaramar763 <pawaramar763@gmail.com>
2023-05-05 18:10:16 +0530

    variable assign
    variable assign

---
+++ 01_variable_assign.js
@@ -0,0 +1,32 @@
+var myFullName;
+console.log(myFullName);
+
+myFullName = "Amar Pawar";
+console.log(myFullName);
+
+myFullName = "Amar Siddheshwar Pawar";
+console.log(myFullName);
+
+var myLove = "I Love only JS";
+console.log(myLove);
+
+var firstName = "Amar";
+console.log(firstName);
+
+var lastName = "Pawar";
+console.log(lastName);
+
+var age = "23";
+console.log(age);
+
+var address = "Ichalkaranji, Kolhapur";
+console.log(address);
+
+var pincode = "416115";
+console.log(pincode);
+
+var gender = "Male";
+console.log(gender);
+
+var isMarried = "No";
+console.log(isMarried);
