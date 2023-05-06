b3ae1888a154f1d1b75be4626585ae08fa852d39
pawaramar763 <pawaramar763@gmail.com>
2023-05-06 21:30:36 +0530

    Variable basic assignment
    Variable basic assignment

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,22 @@
+var rollNo = 12;
+console.log("Roll number:", rollNo, "Type:", typeof rollNo);
+var age = 30;
+console.log("Age:", age, "Type:", typeof age);
+var jsVersion = 5.8;
+console.log("JS version:", jsVersion, "Type:", typeof jsVersion);
+console.log("=========================");
+
+var isPlayer = true;
+console.log("Is player?", isPlayer, "Type:", typeof isPlayer);
+var isHonest = true;
+console.log("Is honest?", isHonest, "Type:", typeof isHonest);
+var isMarried = false;
+console.log("Is married?", isMarried, "Type:", typeof isMarried);
+console.log("=========================");
+
+var country = "India";
+console.log("Country:", country, "Type:", typeof country);
+var state = "Maharashtra";
+console.log("State:", state, "Type:", typeof state);
+var sentence = "I love my India";
+console.log("Sentence:", sentence, "Type:", typeof sentence);
