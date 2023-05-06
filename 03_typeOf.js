2295d27f9976a6422b704c8bb34644ac73606827
pawaramar763 <pawaramar763@gmail.com>
2023-05-06 21:31:13 +0530

    Data types with Variables
    Data types with Variables

---
+++ 03_typeOf.js
@@ -0,0 +1,33 @@
+var bankName;
+console.log(
+  "Before initialization->",
+  "Bank name:",
+  bankName,
+  "Type:",
+  typeof bankName
+);
+bankName = "SBI Bank";
+console.log(
+  "After initialization->",
+  "Bank name:",
+  bankName,
+  "Type:",
+  typeof bankName
+);
+console.log("=============================================================");
+
+var marks = "90%";
+console.log("Type:", typeof marks, "Marks:", marks);
+console.log("=============================================================");
+
+var isWorking;
+console.log("Before initialization->", "is working?", isWorking);
+isWorking = true;
+console.log("After initialization->", "is working?", isWorking);
+console.log("=============================================================");
+
+var totalCount = "Hundred and Seven";
+console.log("Total Count:", totalCount, "Type:", typeof totalCount);
+
+var totalCount = 107;
+console.log("Total Count:", totalCount, "Type:", typeof totalCount);
