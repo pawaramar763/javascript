ee90836235dcb9dabd96d3c218eef14fa10e3edd
pawaramar763 <pawaramar763@gmail.com>
2023-05-11 22:30:30 +0530

    Swapping variables
    Swapping variables

---
+++ 02_swapVariables.js
@@ -0,0 +1,48 @@
+var sweety = "Sweety",
+  cutie = "Cutie";
+console.log(
+  "Values before swapping:",
+  "first name=",
+  sweety,
+  ":second name=",
+  cutie
+);
+var temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log(
+  "Values after swapping:",
+  "first name=",
+  sweety,
+  ":second name=",
+  cutie
+);
+console.log(
+  "======================================================================="
+);
+
+var num1 = 100,
+  num2 = 200,
+  num3 = 300;
+console.log(
+  "Values before swapping:",
+  "first num=",
+  num1,
+  ":second num=",
+  num2,
+  ":third numb=",
+  num3
+);
+var temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log(
+  "Values after swapping:",
+  "first num=",
+  num1,
+  ":second num=",
+  num2,
+  ":third numb=",
+  num3
+);
