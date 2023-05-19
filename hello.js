1758dea93adae1753d7495471b53bda77b0d631e
pawaramar763 <pawaramar763@gmail.com>
2023-05-19 19:32:48 +0530

    Grade assignment using if else if
    Grade assignment using if else if

---
+++ hello.js
@@ -0,0 +1,38 @@
+
+
+
+console.log("Amar Pawar");
+console.log("Ichalkaranji,Kolhapur.");
+var firstName ="Amar..";
+console.log(firstName);
+
+var age; // Variable Declaration 
+age = "22";  // Variable Initialization
+console.log(age);
+
+var holder; // Variable Declaration
+console.log(holder); // undefined
+holder = "My Spec"; // Variable Initialization
+console.log(holder);
+
+holder = "Sachin Spec"; // Variable update
+console.log(holder); //
+
+var number;
+console.log(number);
+console.log(typeof number);
+
+var myName="Amar",lastName="pawar";
+console.log(myName,lastName);
+
+var rollNo=10; var seatNo="ten";
+console.log(rollNo,seatNo);
+
+
+var numOne=100,numTwo=200;
+console.log("before swapping",numOne,numTwo);
+var temp=numOne;
+numOne=numTwo;
+numTwo=temp;
+console.log("after swapping",numOne,numTwo);
+
