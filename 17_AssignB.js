db9ec5c5340ae0f57b6e2abf17a07590e27534a5
pawaramar763 <pawaramar763@gmail.com>
2023-06-05 23:20:08 +0530

    class Employee with use of for loop
    class Employee with use of for loop

---
+++ 17_AssignB.js
@@ -0,0 +1,96 @@
+class Employee {
+  constructor(emp_id, emp_name, emp_dept, emp_salary, emp_company) {
+    this.emp_id = emp_id;
+    this.emp_name = emp_name;
+    this.emp_dept = emp_dept;
+    this.emp_salary = emp_salary;
+    this.emp_company = emp_company;
+  }
+}
+const emp_anil = new Employee(22, "Anil", "IT", 50000, "TCS");
+const emp_radha = new Employee(33, "Radha", "HR", 74000, "Wipro");
+const emp_rishi = new Employee(55, "Rishi", "Finance", 47000, "TCS");
+const emp_sonali = new Employee(66, "Sonali", "Finance", 45000, "Infy");
+const emp_monika = new Employee(77, "Monika", "IT", 40000, "Wipro");
+const emp_viny = new Employee(88, "Vinayak", "IT", 75000, "TCS");
+const emp_mahi = new Employee(99, "Mahesh", "HR", 85000, "Infy");
+
+const array_employess = [
+  emp_anil,
+  emp_radha,
+  emp_rishi,
+  emp_sonali,
+  emp_monika,
+  emp_viny,
+  emp_mahi,
+];
+console.log(`1.All employees working in TCS:`);
+for (const element of array_employess) {
+  if (element.emp_company == "TCS") {
+    console.log(
+      `Employee name is: ${element.emp_name} , Company name: ${element.emp_company}`
+    );
+  }
+}
+console.log(
+  `--------------------------------------------------------------------------------------------`
+);
+
+console.log(`2.Employees whose department is Finance:`);
+for (const element of array_employess) {
+  if (element.emp_dept == "Finance") {
+    console.log(
+      `Department name: ${element.emp_dept} , Employee name: ${element.emp_name}`
+    );
+  }
+}
+console.log(
+  `--------------------------------------------------------------------------------------------`
+);
+
+console.log(`3.The Employees whose name starts with R:`);
+for (const element of array_employess) {
+  if (element.emp_name.startsWith("R")) {
+    console.log(
+      `Employee Id: ${element.emp_id}, Name: ${element.emp_name}, Dept: ${element.emp_dept}, Salary: ${element.emp_salary}, Company: ${element.emp_company}`
+    );
+  }
+}
+console.log(
+  `--------------------------------------------------------------------------------------------`
+);
+
+console.log(`4.Employees Whose salary is greater than 75000:`);
+for (const element of array_employess) {
+  if (element.emp_salary > 75000) {
+    console.log(
+      `Employee name is: ${element.emp_name}, Company: ${element.emp_company}, Salary: ${element.emp_salary}`
+    );
+  }
+}
+console.log(
+  `--------------------------------------------------------------------------------------------`
+);
+
+console.log(
+  `5.Employees Whose salary is greater than or equal to 50000 & from IT Department:`
+);
+for (const element of array_employess) {
+  if (element.emp_salary >= 50000 && element.emp_dept == "IT") {
+    console.log(
+      `Employee Id: ${element.emp_id}, Name: ${element.emp_name}, Dept: ${element.emp_dept}, Salary: ${element.emp_salary}, Company: ${element.emp_company}`
+    );
+  }
+}
+console.log(
+  `--------------------------------------------------------------------------------------------`
+);
+
+console.log(`6.All Employees from Infy:`);
+for (const element of array_employess) {
+  if (element.emp_company == "Infy") {
+    console.log(
+      `Employee Id: ${element.emp_id}, Name: ${element.emp_name}, Dept: ${element.emp_dept}, Salary: ${element.emp_salary}, Company: ${element.emp_company}`
+    );
+  }
+}
